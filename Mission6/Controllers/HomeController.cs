f4692f2b640fd16fafffa963a24f969a331155fd
jessi472 <79349080+jessi472@users.noreply.github.com>
2022-02-07 12:59:14 -0700

    Clesn up unnecessary files

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -11,12 +11,7 @@ namespace Mission6.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
@@ -28,10 +23,6 @@ namespace Mission6.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
     }
 }
