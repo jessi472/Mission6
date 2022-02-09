1ce7828ce490d76f18dec52478506ead3b415b3e
Jacob D <jwdonaldson99@gmail.com>
2022-02-08 19:09:28 -0700

    Models, Context, Etc

---
+++ Mission6/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int CategoryId { get; set; }
+        public string CategoryTitle { get; set; }
+    }
+}
---
+++ Mission6/Models/CoveyForm.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models
+{
+    public class CoveyForm
+    {
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required]
+        public string Task { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public string Quadrant { get; set; }
+       
+        public bool Completed { get; set; }
+
+        //Foreign Category Key
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
